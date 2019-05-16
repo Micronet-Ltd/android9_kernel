ebf68522ec9d0c49b67ba90ff2bb9160956e4b81
barak <barakavigdory@gmail.com>
2019-05-16 20:48:18 +0300

    fixxed a bug with the length check at vgpio read

--- msm-3.18/drivers/gpio/virtual_gpio.c
+++ msm-3.18/drivers/gpio/virtual_gpio.c
@@ -328,7 +328,7 @@ struct virt_gpio *dev = file->private_data;
 	output[1] = (uint8_t)GPIO_INT_STATUS; //default, this place in the data should signal the ioriver/control
 										  //which kind of operation should it does
 	pr_err("%s() READ\n", __func__);
-	if (count < sizeof(output))
+	if (count < w)
 	{
 		pr_err("%s() count too small\n", __func__);
 		return -EINVAL;
