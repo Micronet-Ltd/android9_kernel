366f1abded7231e40ba159e1aa89efc95307e158
barak <barakavigdory@gmail.com>
2019-06-06 15:08:54 +0300

    fixed the return value at the J1708 and rs485

--- msm-3.18/drivers/switch/switch_dock.c
+++ msm-3.18/drivers/switch/switch_dock.c
@@ -754,7 +754,9 @@ static ssize_t rs485_en_state_show(struct device *dev, struct device_attribute *
     struct switch_dev *sdev = (struct switch_dev *)dev_get_drvdata(dev);
     struct dock_switch_device *ds = container_of(sdev, struct dock_switch_device, sdev);
 
-    return gpio_get_value_cansleep(ds->mcu_outs_pins[RS48_GPIO_OFFSET]);
+    *buf = gpio_get_value_cansleep(ds->mcu_outs_pins[RS48_GPIO_OFFSET]);
+
+    return 1;
 }
 
 static ssize_t rs485_en_state_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
@@ -774,7 +776,9 @@ static ssize_t j1708_en_state_show(struct device *dev, struct device_attribute *
     struct switch_dev *sdev = (struct switch_dev *)dev_get_drvdata(dev);
     struct dock_switch_device *ds = container_of(sdev, struct dock_switch_device, sdev);
  
-    return gpio_get_value_cansleep(ds->mcu_outs_pins[J1708_GPIO_OFFSET]);
+    *buf = gpio_get_value_cansleep(ds->mcu_outs_pins[J1708_GPIO_OFFSET];
+
+    return 1;
 }
 
 static ssize_t j1708_en_state_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
