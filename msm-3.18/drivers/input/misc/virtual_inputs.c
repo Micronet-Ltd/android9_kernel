f6eb29b151e98d4b7d6db18efe3a6b13439cfeb2
mikhail.efimov <mikhail.efimov@micronet-inc.com>
2019-11-18 15:36:52 +0200

    added branch for smartcam

--- msm-3.18/drivers/input/misc/virtual_inputs.c
+++ msm-3.18/drivers/input/misc/virtual_inputs.c
@@ -7,6 +7,9 @@
 #include <linux/miscdevice.h>
 #include <linux/notifier.h>
 #include <linux/gpio.h>
+#include <linux/of.h>//Michael Efimov: added
+//#include <linux/workqueue.h>//Michael Efimov: added
+//#include <linux/of_address.h>//Michael Efimov: added
 
 #include <linux/syscalls.h>
 #include <linux/file.h>
@@ -14,11 +17,13 @@
 #include <linux/dirent.h>
 #include <linux/string.h>
 #include <linux/hwmon-sysfs.h>
+#include "../../misc/hi_3w/hi_3w.h"//Michael Efimov: added
 
 #define VINPUTS_NAME	"vinputs"
 #define	FIND_NAME 		"vgpio_in"
 
 extern int32_t gpio_in_register_notifier(struct notifier_block *nb);
+extern int cradle_register_notifier(struct notifier_block *nb);//Michael Efimov: added
 
 struct gpio_set {
 	int base;
@@ -57,11 +62,14 @@ struct virt_inputs {
 	struct 	miscdevice*	mdev;
 	struct 	input_dev* 	input_dev;
 	struct  work_struct work;
+    struct  delayed_work    virtual_input_init_work;//Michael Efimov:  added
+    struct  notifier_block virtual_inputs_cradle_notifier;//Michael Efimov:  added
 	struct 	work_params wparams; 
 	struct 	vinput_key* vmap;
 	struct	gpio_set 	gpios_in;
 	struct 	notifier_block   notifier;
 	int		reinit;
+    int     cradle_attached;//Michael Efimov:  added
     struct mutex lock;
 };
 
@@ -216,6 +224,73 @@ static void vinputs_work_func(struct work_struct *work)
 	}
 }
 
+//***       Michael Efimov: added       ***//
+static void cradle_is_connected_work_fix(struct work_struct *work){
+    uint32_t cmd = 0/*, inputs_status = 0*/;
+    uint32_t inp_val[2] = {0};
+    int temp_val = 0, sync = 0, /*number_chnl = 0,*/ cnt, strt_cnt, stp_cnt;
+
+    struct virt_inputs *vinp = container_of(work, struct virt_inputs, virtual_input_init_work.work);
+    if (vinp->cradle_attached) {
+        strt_cnt = 0;
+        stp_cnt = 2;
+        /*hi_3w_tx_cmd(&cmd, 1);
+        if (cmd & 0x18) {
+            inputs_status = 1;
+            if ((cmd & 0x18) == 0x08) {
+                stp_cnt = 1;
+                number_chnl = 2;
+                pr_notice("first chnl %x\n", (cmd & 0x18)); //Michael Efimov: added for test
+            }
+            if ((cmd & 0x18) == 0x10) {
+                strt_cnt = 1;
+                number_chnl = 1;
+                pr_notice("second chnl %x\n", (cmd & 0x18));//Michael Efimov: added for test
+            }
+            if ((cmd & 0x18) == 0x18) {
+                pr_notice("both chnls %x\n", (cmd & 0x18));//Michael Efimov: added for test
+            }
+            if (number_chnl) {
+                temp_val = 0;
+                if (vinp->vmap[number_chnl - 1].val != temp_val) {
+                    input_report_abs(vinp->input_dev, vinp->vmap[number_chnl - 1].code, temp_val);
+                    vinp->vmap[number_chnl - 1].val = temp_val;
+                    pr_notice("value key%d-%d\n",cnt+1 , inp_val[number_chnl - 1]);//Michael Efimov: added for test
+                    sync = 1;
+                }
+            }
+        }*/
+        for (cnt = strt_cnt; cnt < stp_cnt; cnt++) {
+            //if (inputs_status) {
+                cmd = cnt << 2;
+                cmd|=0x28;
+                cmd<<=24;
+                pr_notice("request command%d %x\n", (cnt+1), cmd);//Michael Efimov: added for test
+                hi_3w_tx_cmd(&cmd, 1);
+                pr_notice("responce command%d %x\n", (cnt+1), cmd);//Michael Efimov: added for test
+                inp_val[cnt] = cmd;
+                inp_val[cnt] &=~(0xFF000000);
+                pr_notice("value%d-%d\n",cnt+1 , inp_val[cnt]);//Michael Efimov: added for test
+                temp_val = (inp_val[cnt]>6000) ? (KEY_F1 + cnt) : 0;
+            //}
+            if (vinp->vmap[cnt].val!=temp_val) {
+                input_report_abs(vinp->input_dev, vinp->vmap[cnt].code, temp_val);
+                vinp->vmap[cnt].val = temp_val;
+                pr_notice("value key%d-%d\n",cnt+1 , inp_val[cnt]);//Michael Efimov: added for test
+                sync = 1;
+            }
+        }
+        if (sync) {
+            input_sync(vinp->input_dev);
+            pr_notice("was sinchroniced\n");//Michael Efimov: added for test
+        }
+        pr_notice("responce command1 %d\n", inp_val[0]);//Michael Efimov: added for test
+        pr_notice("responce command2 %d\n", inp_val[1]);//Michael Efimov: added for test
+        schedule_delayed_work(&vdev->virtual_input_init_work, msecs_to_jiffies(1000));
+    }
+}
+//****************************************//
+
 static int32_t __ref vinputs_callback(struct notifier_block *nfb, unsigned long reason, void *arg)
 {
     struct virt_inputs *vinp = container_of(nfb, struct virt_inputs, notifier);
@@ -234,6 +309,22 @@ static int32_t __ref vinputs_callback(struct notifier_block *nfb, unsigned long 
 
     return NOTIFY_OK;
 }
+
+//***                                           Michael Efimov: added                                           ***//
+static int __ref virtual_inputs_cradle_callback(struct notifier_block *nfb, unsigned long reason, void *p)
+{
+    struct virt_inputs *vinputs = container_of(nfb, struct virt_inputs, virtual_inputs_cradle_notifier);
+
+    vinputs->cradle_attached = reason;
+    if (vinputs->cradle_attached) {
+        cancel_delayed_work(&vdev->virtual_input_init_work);
+        schedule_delayed_work(&vdev->virtual_input_init_work, 0);
+    }else{
+        cancel_delayed_work(&vdev->virtual_input_init_work);
+    }
+	return NOTIFY_OK;
+}
+//****************************************************************************************************************//
 
 static int vinputs_open(struct inode *inode, struct file *file)
 {
@@ -508,8 +599,23 @@ static int vinputs_create_input_dev(struct virt_inputs* vdev)
 static int __init virtual_inputs_init(void)
 {
 	int error;
+    int fixed_mode = 0;//Michael Efimov:  added
 	struct device *dev;
+    struct device_node *np;//Michael Efimov:  added
 
+    //***          Michael Efimov:       added         ***//
+    np = of_find_compatible_node(NULL, NULL, "mcn,fixed-vinputs");
+    if (np) {
+        fixed_mode = 1;
+        pr_err("node is finded\n");
+    }
+    /*if (!np) {
+        pr_err("driver is not finded\n");//Michael Efimov: added for test
+    } else {
+        fixed_mode = 1;
+        pr_err("driver is finded\n");//Michael Efimov: added for test
+    }*/
+    //**************************************************//
 	pr_info("%s:+\n", __func__);
 	vdev = kzalloc(sizeof(struct virt_inputs), GFP_KERNEL);
 	if (!vdev)
@@ -523,41 +629,57 @@ static int __init virtual_inputs_init(void)
 		pr_err("%s: misc_register failed\n", vinputs_dev.name);
 		return -EINVAL;
 	}
-	vdev->mdev = &vinputs_dev;
-	vdev->notifier.notifier_call = vinputs_callback;
-	error = gpio_in_register_notifier(&vdev->notifier);
-	if (error) {
-		pr_err("failure to register remount notifier [%d]\n", error);
-		misc_deregister(&vinputs_dev);
-		kfree(vdev);
-		return error;
-	}
 
+    //***          Michael Efimov:  added         ***//
+    if (fixed_mode) {
+        vdev->virtual_inputs_cradle_notifier.notifier_call = virtual_inputs_cradle_callback;//put pointer on the callback func to the notifier block
+        cradle_register_notifier(&vdev->virtual_inputs_cradle_notifier);//register callback func in cradle notifier
+        INIT_DELAYED_WORK(&vdev->virtual_input_init_work, cradle_is_connected_work_fix);//initial working func in bill of delayed work
+        vdev->cradle_attached = 0;//initial var in structure for first check in the wirking func
+        schedule_delayed_work(&vdev->virtual_input_init_work, 0);//run working func without delayer in first time
+
+        //pr_info("%s:-\n", __func__);
+
+        //return 0;
+    }
+    //*************************************************//
+
+	vdev->mdev = &vinputs_dev;
+    if(!fixed_mode){//Michael Efimov:  added - if(!fixed_mode){
+        vdev->notifier.notifier_call = vinputs_callback;
+        error = gpio_in_register_notifier(&vdev->notifier);
+        if (error) {
+            pr_err("failure to register remount notifier [%d]\n", error);
+            misc_deregister(&vinputs_dev);
+            kfree(vdev);
+            return error;
+        }
+    }//Michael Efimov:  added - }
 	error = vinputs_create_input_dev(vdev);
 	if (error) {
 		misc_deregister(&vinputs_dev);
 		kfree(vdev);
 		return error;
 	}
-
-	dev = vdev->mdev->this_device;
-	error = sysfs_create_group(&dev->kobj, &in_attr_group);
-	if (error) {
-		pr_err("%s: could not create sysfs group\n", __func__);
-		input_free_device(vdev->input_dev);
-		misc_deregister(&vinputs_dev);
-		kfree(vdev);
-		return error;
-	}
-
+    //if(!fixed_mode){//Michael Efimov:  added - if(!fixed_mode){
+        dev = vdev->mdev->this_device;
+        error = sysfs_create_group(&dev->kobj, &in_attr_group);
+        if (error) {
+            pr_err("%s: could not create sysfs group\n", __func__);
+            input_free_device(vdev->input_dev);
+            misc_deregister(&vinputs_dev);
+            kfree(vdev);
+            return error;
+        }
+    //}//Michael Efimov:  added - }
     mutex_init(&vdev->lock);
     vdev->reinit = 1;
-	INIT_WORK(&vdev->work, vinputs_work_func);
-//	vinputs_init_files();
-    schedule_work(&vdev->work); 
-
-	pr_info("%s:-\n", __func__);
-
+    if(!fixed_mode){//Michael Efimov:  added - if(!fixed_mode){
+        INIT_WORK(&vdev->work, vinputs_work_func);
+        //	vinputs_init_files();
+        schedule_work(&vdev->work); 
+    }//Michael Efimov:  added - }
+    pr_info("%s:-\n", __func__);
 	return 0;
 }
 
